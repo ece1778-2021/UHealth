4e33abeef6c95ced21ddcc3dcb58e0785d365b00
Alan <memoalanoo1@msn.cn>
2021-02-23 18:35:51 -0500

    init

---
+++ Uhealth/app/src/main/java/com/example/uhealth/InitRegPage.java
@@ -0,0 +1,14 @@
+package com.example.uhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitRegPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init_reg_page);
+    }
+}
