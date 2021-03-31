931b2d8a8888f0500f9dee467c498a6b89f11dcf
Alan <memoalanoo1@msn.cn>
2021-03-30 20:48:09 -0400

    shareml3

--- Uhealth/app/src/main/java/com/example/uhealth/Fragments/ShareView_frag.java
+++ Uhealth/app/src/main/java/com/example/uhealth/Fragments/ShareView_frag.java
@@ -1,17 +1,24 @@
 package com.example.uhealth.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.uhealth.R;
+import com.example.uhealth.ViewModel.Share_ViewModel;
 
 public class ShareView_frag extends Fragment {
+    private Context mContext;
+    private Share_ViewModel viewModel;
+
 
     public ShareView_frag() {
         // Required empty public constructor
@@ -31,7 +38,14 @@ public class ShareView_frag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share_view_frag, container, false);
+        View view = inflater.inflate(R.layout.fragment_share_view_frag, container, false);
+        return view;
     }
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+        viewModel = new ViewModelProvider(getActivity()).get(Share_ViewModel.class);
+    }
 }
