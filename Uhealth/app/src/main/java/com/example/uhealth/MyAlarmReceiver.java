38ea729917f98bbe53f1c2d2fbdc5881dc98689d
eeskimol <877965280@qq.com>
2021-04-11 15:00:50 +0800

    notification added

--- Uhealth/app/src/main/java/com/example/uhealth/MyAlarmReceiver.java
+++ Uhealth/app/src/main/java/com/example/uhealth/MyAlarmReceiver.java
@@ -1,5 +1,8 @@
 package com.example.uhealth;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -11,18 +14,40 @@ import androidx.appcompat.app.AlertDialog;
 
 public class MyAlarmReceiver extends BroadcastReceiver {
     AlertDialog mAlarmAppointmentDialog;
-
+    private NotificationManager m_notificationMgr = null;
+    private static final int NOTIFICATION_FLAG = 3;
     @Override
     public void onReceive(Context context, Intent intent) {
         //String AppointmentType = intent.getStringExtra("type");
-
+        //
+        m_notificationMgr = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        //
         Intent starter = intent;
         Bundle extras = starter.getExtras();
         if (extras != null) {
             boolean isAppointment = extras.getBoolean("isAppointment", false);
             boolean isMedication= extras.getBoolean("isMedication", false);
             if (isAppointment) {
-                //-----------------------------------------
+                Intent ListIntent = new Intent(context, AppointmnetList.class);
+                ListIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                ListIntent.putExtra("isAppointment",true);
+                ListIntent.putExtra("typefromalarm","AppointmentReminder");
+                ListIntent.putExtra("apttype",extras.getString("apttype"));
+                ListIntent.putExtra("apttime",extras.getString("apttime"));
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, ListIntent, 0);
+                Notification notify = new Notification.Builder(context)
+                        .setSmallIcon(R.drawable.ic_baseline_favorite_24)
+                        .setTicker(extras.getString("apttype"))
+                        .setContentTitle("Appointment Reminder!")
+                        .setContentText(extras.getString("apttime"))
+                        .setContentIntent(pendingIntent)
+                        .setNumber(1)
+                        .build();
+                notify.flags |= Notification.FLAG_AUTO_CANCEL;
+                m_notificationMgr .notify(NOTIFICATION_FLAG, notify);
+
+
+                /*-----------------------------------------
                 Intent ListIntent = new Intent(context, AppointmnetList.class);
                 ListIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 ListIntent.putExtra("isAppointment",true);
@@ -30,7 +55,7 @@ public class MyAlarmReceiver extends BroadcastReceiver {
                 ListIntent.putExtra("apttype",extras.getString("apttype"));
                 ListIntent.putExtra("apttime",extras.getString("apttime"));
                 context.startActivity(ListIntent);
-
+                */
 
             } else {
 
